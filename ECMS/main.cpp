58eaa59e0673b221140127587b3844f9052044f9
gug <incisaguglielmo@hotmail.com>
2021-05-28 00:57:42 +0100

    Initial Commit

---
+++ ECMS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ECMS
+//
+//  Created by Guglielmo Incisa di Camerana on 28/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
